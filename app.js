3ab2172cbd06363eb1e9c822fc2375d949ad40fb
dhanubor <nikhilmistry121203@gmail.com>
2025-02-03 16:22:27 +0600

    go

---
+++ app.js
@@ -0,0 +1,20 @@
+let x = 10;
+console.log(x); // Output: 10
+
+x += 5;
+console.log(x); // Output: 15
+
+x -= 3;
+console.log(x); // Output: 12
+
+x *= 2;
+console.log(x); // Output: 24
+
+x /= 4;
+console.log(x); // Output: 6
+
+x %= 5;
+console.log(x); // Output: 1
+
+x **= 3;
+console.log(x); // Output: 1 (কারণ আগের x = 1, তাই 1 ** 3 = 1)
